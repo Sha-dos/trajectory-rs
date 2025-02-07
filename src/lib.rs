4cf0f3158c38fb6d89bd41af5050ee055c96a701
Nolan Peterson <ndpete2007@gmail.com>
2025-02-07 17:33:27 -0600

    new choreo framework

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![feature(btree_cursors)]
+#![feature(btree_cursors, diagnostic_namespace)]
 
 use std::{collections::BTreeMap, process::Output, ops::{Mul, Add, Sub, Deref}};
 
@@ -6,24 +6,29 @@ use ordered_float::NotNan;
 use serde::{Serialize, Deserialize};
 use uom::si::{f64::{AngularVelocity, Length, Angle, Velocity, Time}, length::meter, angle::{radian, degree}, velocity::meter_per_second, angular_velocity::radian_per_second, time::second};
 
+// New root structure to match JSON
+#[derive(Serialize, Deserialize)]
+pub struct TrajectoryFile {
+    trajectory: Trajectory
+}
+
 #[derive(Serialize, Deserialize)]
 pub struct Trajectory {
     samples: Vec<Sample>,
 }
 
-
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Sample {
+    t: f64,  // Changed from timestamp
     x: f64,
     y: f64,
     heading: f64,
-    #[serde(rename = "angularVelocity")]
+    #[serde(rename = "omega")]  // Changed from angular_velocity
     angular_velocity: f64,
-    #[serde(rename = "velocityX")]
+    #[serde(rename = "vx")]  // Changed from velocity_x
     velocity_x: f64,
-    #[serde(rename = "velocityY")]
+    #[serde(rename = "vy")]  // Changed from velocity_y
     velocity_y: f64,
-    timestamp: f64,
 }
 
 pub struct Path {
@@ -32,7 +37,8 @@ pub struct Path {
 
 impl Path {
     pub fn from_trajectory(trajectory: &str) -> Result<Self, serde_json::Error> {
-        Ok(serde_json::from_str::<Trajectory>(trajectory)?.into())
+        // Updated to handle the new root structure
+        Ok(serde_json::from_str::<TrajectoryFile>(trajectory)?.trajectory.into())
     }
 
     pub fn get(&self, elapsed: Time) -> Pose {
@@ -40,14 +46,16 @@ impl Path {
         let below = self.samples.upper_bound(std::ops::Bound::Included(&NotNan::new(elapsed).unwrap()));
 
         if let Some(above) = below.peek_next() {
-            let progress = elapsed / *(above.0 - below.key().unwrap());
+            let below_time = **below.key().unwrap();
+            let above_time = **above.0;
+            let progress = (elapsed - below_time) / (above_time - below_time);
 
             below.value().unwrap().lerp(above.1, progress)
-
         } else {
             below.value().unwrap().to_owned()
         }
     }
+
     pub fn length(&self) -> Time {
         Time::new::<second>(**self.samples.last_key_value().unwrap().0)
     }
@@ -55,12 +63,11 @@ impl Path {
 
 impl From<Trajectory> for Path {
     fn from(value: Trajectory) -> Self {
-        let mut map : BTreeMap<NotNan<f64>, Pose>= BTreeMap::new();
+        let mut map: BTreeMap<NotNan<f64>, Pose> = BTreeMap::new();
         for sample in value.samples {
-            map.insert(NotNan::new(sample.timestamp).unwrap(),sample.into());
+            map.insert(NotNan::new(sample.t).unwrap(), sample.into());  // Changed from timestamp to t
         }
-
-        Self{ samples: map}
+        Self { samples: map }
     }
 }
 
@@ -74,14 +81,12 @@ pub struct Pose {
     pub velocity_y: Velocity,
 }
 
-
 fn lerp<A>(a: A, b: A, l: f64) -> A
-where A: Sub<A, Output=A> +Add<A, Output=A> + Mul<f64, Output = A> + Clone {
-    a.clone() + (b.clone()-a.clone())*l
+where A: Sub<A, Output=A> + Add<A, Output=A> + Mul<f64, Output = A> + Clone {
+    a.clone() + (b - a) * l
 }
 
 impl Pose {
-
     fn lerp(&self, other: &Pose, l: f64) -> Pose {
         Pose {
             x: lerp(self.x, other.x, l),
@@ -91,14 +96,12 @@ impl Pose {
             velocity_x: lerp(self.velocity_x, other.velocity_x, l),
             velocity_y: lerp(self.velocity_y, other.velocity_y, l),
         }
-        
     }
 
     pub fn mirror(&mut self) {
-        self.x = Length::new::<meter>(8.)- self.x;
-        self.heading = Angle::new::<degree>(90.)- self.heading;
+        self.x = Length::new::<meter>(8.) - self.x;
+        self.heading = Angle::new::<degree>(90.) - self.heading;
     }
-    
 }
 
 impl From<Sample> for Pose {
@@ -120,8 +123,8 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let deser: Trajectory = serde_json::from_str(include_str!("../../deploy/choreo/center-long.traj")).unwrap();
-        println!("samp 1 {:?}", deser.samples[0]);
-        println!("sec 1.5 {:?}", Path::from(deser).get(Time::new::<second>(1.5)));
+        let trajectory_file = include_str!("../../RobotCode2025/auto/BlueTriangle.traj");
+        let path = Path::from_trajectory(trajectory_file).unwrap();
+        println!("sec 1.5 {:?}", path.get(Time::new::<second>(1.5)));
     }
 }
